5b46ae9aa769fcc6f09ebcac7cb039ae7c7b3041
Nickolas Humeniuk <kolin98111@gmail.com>
2017-06-20 03:04:48 +0300

    Improved design

--- app/src/main/java/com/example/nickolas/simplemessage/Registration.java
+++ app/src/main/java/com/example/nickolas/simplemessage/Registration.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,7 +35,8 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
         Slidr.attach(this);
         setContentView(R.layout.activity_registration);
         getSupportActionBar().setTitle(getResources().getString(R.string.registration));
-
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         photo = (ImageView) findViewById(R.id.registration_photo);
 
         name = (EditText) findViewById(R.id.registration_name);
@@ -76,6 +78,14 @@ public class Registration extends AppCompatActivity implements View.OnClickListe
                 setPhoto();
                 break;
         }
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return true;
     }
 
     @Override
